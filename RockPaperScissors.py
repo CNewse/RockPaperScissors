9efa3a8d4d3587567032544b6c526b30474bf36f
Chris Newson <chrisnewson16@gmail.com>
2018-08-26 16:50:48 +0100

    create module for determining result

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,35 +1,35 @@
-
 ''' ROCK PAPER SCOSSORS '''
 ''' C NEWSON 26/08/2018 '''
 
 import random
 
-
+## Set game rules
+def who_wins():  
+    if ai == user_weapon:
+        result = 'DRAW'
+    elif ai == 'scissors':
+        if user_weapon == 'rock':
+            result = 'you win!'
+        elif user_weapon == 'paper':
+            result = 'you lose'
+    elif ai == 'paper':
+        if user_weapon == 'rock':
+            result = 'you lose'
+        elif user_weapon == 'scissors':
+            result = 'you win!'
+    elif ai == 'rock':
+        if user_weapon == 'paper':
+            result = 'you win!'
+        elif user_weapn == 'scissors':
+            result = 'you lose'
+    return result
 
 ## Initialise AI
 ai = random.choice(['rock','paper','scissors'])
 
 user_weapon = input('Please select your weapon, rock, paper or scissors: ')
 
-if ai == user_weapon:
-    result = 'DRAW'
-elif ai == 'scissors':
-    if user_weapon == 'rock':
-        result = 'you win!'
-    elif user_weapon == 'paper':
-        result = 'you lose'
-elif ai == 'paper':
-    if user_weapon == 'rock':
-        result = 'you lose'
-    elif user_weapon == 'scissors':
-        result = 'you win!'
-elif ai == 'rock':
-    if user_weapon == 'paper':
-        result = 'you win!'
-    elif user_weapn == 'scissors':
-        result = 'you lose'
-    
 print('You chose', user_weapon)
 print('The computer chose', ai, '\n')
 
-print(result)
+print(who_wins())
