c93c62e53d3f17ce3cd79f2641065eca128d2e06
Chris Newson <chrisnewson16@gmail.com>
2018-08-26 17:42:49 +0100

    multiple game option and summary of results added

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -3,33 +3,49 @@
 
 import random
 
-## Set game rules
+## Set game rules and results (0 = draw, 1 = player wins, -1 = ai win)
 def who_wins():  
-    if ai == user_weapon:
-        result = 'DRAW'
-    elif ai == 'scissors':
+    if ai_weapon == user_weapon:
+        result = 0
+    elif ai_weapon == 'scissors':
         if user_weapon == 'rock':
-            result = 'you win!'
+            result = 1
         elif user_weapon == 'paper':
-            result = 'you lose'
-    elif ai == 'paper':
+            result = -1
+    elif ai_weapon == 'paper':
         if user_weapon == 'rock':
-            result = 'you lose'
+            result = -1
         elif user_weapon == 'scissors':
-            result = 'you win!'
-    elif ai == 'rock':
+            result = 1
+    elif ai_weapon == 'rock':
         if user_weapon == 'paper':
-            result = 'you win!'
+            result = 1
         elif user_weapn == 'scissors':
-            result = 'you lose'
+            result = -1
     return result
 
-## Initialise AI
-ai = random.choice(['rock','paper','scissors'])
+num_games = int(input('How many games would you like to play?'))
 
-user_weapon = input('Please select your weapon, rock, paper or scissors: ')
+##initialise lists to hold game results
+ai_weapons = []
+user_weapons = []
+results = []
 
-print('You chose', user_weapon)
-print('The computer chose', ai, '\n')
+for i in range(num_games):
+    ai_weapon = random.choice(['rock','paper','scissors'])
+    user_weapon = input('\n Please select your weapon, rock, paper or scissors: ')
+    ai_weapons.append(ai_weapon)
+    user_weapons.append(user_weapon)
+    results.append(who_wins())
+    if who_wins() == 1:
+        game_result = 'you win!'
+    elif who_wins() == -1:
+        game_result = 'you lose!'
+    elif who_wins() == 0:
+        game_result = "it's a draw"
 
-print(who_wins())
+    print('The computer chose', ai_weapon, game_result)
+
+agg_results = {x:results.count(x) for x in results}
+
+print('\n','Out of' ,num_games, 'you won', agg_results.get(1,None))
