3970e4a6d5bbddef9a14334ba76468b2b20b2918
Chris Newson <chrisnewson16@gmail.com>
2018-08-26 16:35:45 +0100

    adding random ai choice :

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -2,16 +2,34 @@
 ''' ROCK PAPER SCOSSORS '''
 ''' C NEWSON 26/08/2018 '''
 
+import random
+
+
+
 ## Initialise AI
-ai = 'scissors'
+ai = random.choice(['rock','paper','scissors'])
 
 user_weapon = input('Please select your weapon, rock, paper or scissors: ')
 
-if ai == 'scissors' and user_weapon == 'rock':
-    result = 'player wins'
-elif ai == 'scissors' and user_weapon == 'paper':
-    result = 'computer wins'
-else:
-    result = 'Draw'
+if ai == user_weapon:
+    result = 'DRAW'
+elif ai == 'scissors':
+    if user_weapon == 'rock':
+        result = 'you win!'
+    elif user_weapon == 'paper':
+        result = 'you lose'
+elif ai == 'paper':
+    if user_weapon == 'rock':
+        result = 'you lose'
+    elif user_weapon == 'scissors':
+        result = 'you win!'
+elif ai == 'rock':
+    if user_weapon == 'paper':
+        result = 'you win!'
+    elif user_weapn == 'scissors':
+        result = 'you lose'
+    
+print('You chose', user_weapon)
+print('The computer chose', ai, '\n')
 
 print(result)
